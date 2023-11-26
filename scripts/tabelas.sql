c12255d5154f49c912927f750c25acf3dad2c282
Lugui14 <zanelallopes9977@gmail.com>
2023-11-26 14:29:24 -0300

    fix: banco att

--- scripts/tabelas.sql
+++ scripts/tabelas.sql
@@ -19,7 +19,7 @@ CREATE TABLE produto (
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(500),
     preco DECIMAL(10, 2) NOT NULL,
-    quantidade INTEGER NOT NULL
+    quantidade_estoque INTEGER NOT NULL
 );
 
 CREATE TABLE cliente (
@@ -33,13 +33,19 @@ CREATE TABLE cliente (
 
 CREATE TABLE pedido (
     codp SERIAL PRIMARY KEY,
-    idp INTEGER NOT NULL,
     idc INTEGER NOT NULL,
     idu INTEGER NOT NULL,
     estado VARCHAR(50) NOT NULL,
-    quantidade INTEGER NOT NULL,
     dth_venda TIMESTAMP NOT NULL,
-    CONSTRAINT fk_pedido_produto FOREIGN KEY (idp) REFERENCES produto(idp),
     CONSTRAINT fk_pedido_cliente FOREIGN KEY (idc) REFERENCES cliente(idc),
     CONSTRAINT fk_pedido_usuario FOREIGN KEY (idu) REFERENCES usuario(idu)
+);
+
+CREATE TABLE itempedido (
+    idi SERIAL PRIMARY KEY,
+    codp INTEGER NOT NULL,
+    idp INTEGER NOT NULL,
+    quantidade_pedido INTEGER NOT NULL,
+    CONSTRAINT fk_itempedido_produto FOREIGN KEY (idp) REFERENCES produto(idp),
+    CONSTRAINT fk_itempedido_pedido FOREIGN KEY (codp) REFERENCES pedido(codp)
 );
